666c563c9bce0ee8d6cca4568486b96ee0b961ea
Richard Davies <Richard.Davies@bcu.ac.uk>
2020-02-20 08:33:34 +0000

    Submission for Session 1

---
+++ Activity 2/index.php
@@ -0,0 +1,44 @@
+<?php
+$title = "Test Website";
+$subtitle = "This is a test website that is going to look very pretty";
+$left = "驚異 第三回 不題 了」 德泉淹. 第三回 第二回 第十回 第七回 相域. 去 事 誨 矣 出 覽 意 ，可. 汗流如雨 吉安而來 玉，不題 覽 父親回衙 去 意 冒認收了 誨. 汗流如雨 父親回衙 吉安而來. 關雎 覽 」 誨 意 耳. 」 事 矣 饒爾去罷」 ，愈聽愈惱 此是後話 去 曰： 覽 意 ，可 也懊悔不了. ，愈聽愈惱 此是後話 饒爾去罷」. 誨 建章曰： 」 事 意 訖乃返 覽 曰： 在一處 分得意 後竊聽. ，可 誨 關雎 耳 矣 出 曰： 事. 」 覽 去 矣 ，可 出 誨 關雎. 第八回 第二回 第六回 第三回 第七回 第九回. 去 誨 」 關雎 出 意 曰： 耳. 耳 曰： 出 矣 」. ";
+$middle = " 出 去 矣 」 耳 ，可. 饒爾去罷」 也懊悔不了 ，愈聽愈惱 此是後話. 矣 關雎 事 」 誨. 耳 誨 ，可 意 關雎 事 矣. 第八回 事 貢院 了」 第九回 矣 第三回 耳 第一回 關雎 覽. ，愈聽愈惱 也懊悔不了 饒爾去罷」 此是後話. 意 覽 耳 事 ，可 去 矣. 後竊聽 在一處 危德至. 分得意 不稱讚 建章曰：. 父親回衙 玉，不題 冒認收了 汗流如雨. 去 矣 ，可 關雎 意 出. 第五回 驚異 第九回 不題 第三回. ，可 覽 耳 關雎 曰： 矣 誨 事. 玉，不題 父親回衙 冒認收了 汗流如雨 吉安而來. 覽 耳 意 事 曰： 關雎 ，可 去. ，可 曰： 冒認收了 」 汗流如雨 出 吉安而來 玉，不題 意. 第一回 曰： 貢院 招」 第三回 第二回 誨 耳 驚異 意 」 覽 ，可. 
+
+了」 第四回 第十回 貢院 相域 德泉淹. 此是後話 也懊悔不了 ，愈聽愈惱 饒爾去罷」. 覽 出 ，可 去 關雎. 冒認收了 吉安而來 玉，不題 父親回衙. 冒認收了 汗流如雨 父親回衙 玉，不題 吉安而來. 覽 意 ，可 出 誨 」 曰：. 意 事 曰： 覽 出. 意 耳 矣 覽 出 」 去. 去 關雎 饒爾去罷」 覽 矣 也懊悔不了 此是後話 ，愈聽愈惱 事. 去 曰： 關雎 」 出 ，可. 不稱讚 分得意 後竊聽 訖乃返 危德至. 意 驚異 去 第九回 」 關雎 不題 耳 羨殺 第一回 第二回 ，可. 
+";
+$right = "以測機 建章曰： 第十一回. 誨 」 矣 覽 ，可. 汗流如雨 玉，不題 吉安而來. 第二回 ，可 覽 」 出 不題 矣 羨殺 誨. 事 」 矣 ，可 第四回 招」 第三回 第一回 誨 覽 羨殺 出 第二回. 」 ，可 出 意. 分得意 訖乃返 ，可 出 覽 」 在一處 事 後竊聽 曰： 意 去 己轉身. 誨 去 曰： 事 矣. 關雎 去 耳 覽 曰：. 出 後竊聽 危德至 分得意 矣 不稱讚 耳 第十一回 」 去. ，愈聽愈惱 也懊悔不了 饒爾去罷」 此是後話. ";
+?>
+
+<html> 
+	<head>
+		<title><?php echo $title;?></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	  	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-12 col-lg-12">
+					<?php echo "<h1>".$title."</h1><br><h3>".$subtitle."</br>";?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-3 col-lg-2">
+					<?php echo $left;?>
+				</div>
+				<div class="col-12 col-md-6 col-lg-8">
+					<?php echo $middle;?>
+				</div>
+				<div class="col-12 col-md-3 col-lg-2">
+					<?php echo $right;?>
+				</div>
+
+
+			</div>
+		</div>
+	</body>
+
+</html>
+
