10c8aa9ce122a8618f5ae3ecdf6b6103b6154bdf
Richard Davies <Richard.Davies@bcu.ac.uk>
2020-02-21 08:36:56 +0000

    Homework Submission
    This is my home work submission becuase I am super cool.

---
+++ Activity 2/index.php
@@ -0,0 +1,52 @@
+<?php
+$title = "Test Page - Group 3";
+$subtitle = "Welcome Group 3, I miss you all";
+$left = " <strong>覽 出 誨 矣 」 意. ，可 出 關雎 事</strong> 覽 誨. 驚異 了」 第六回 貢院 第十回. 矣 出 事 意 耳 誨 關雎. 分得意 在一處 訖乃返 危德至 以測機. 第十一回 樂而不淫 危德至 ﻿白圭志 不稱讚. 饒爾去罷」 也懊悔不了 此是後話. 意 訖乃返 關雎 曰： ，可 覽 分得意 出 事 ﻿白圭志. 出 去 關雎 事 曰： 誨 」. 冒認收了 玉，不題 父親回衙 吉安而來. 父親回衙 汗流如雨 玉，不題 冒認收了.</br></br> ";
+$right = " <strong>事 出 誨 矣. 也懊悔不了 ，愈聽愈惱</strong> 饒爾去罷」. 羨殺 第十回 第八回 相域 第一回 貢院. 耳 事 矣 ，可 覽 父親回衙 關雎 吉安而來 玉，不題 」 冒認收了 汗流如雨. 事 誨 」 ，可 耳 去 意 關雎. 樂而不淫 訖乃返 危德至 分得意 ﻿白圭志. 也懊悔不了 ，愈聽愈惱 饒爾去罷」 此是後話. 誨 ，可 去 覽 出 耳 」 矣. 去 」 ，可 事 出 曰： 關雎 意. 汗流如雨 冒認收了 出 父親回衙 矣 耳 意 覽 關雎 吉安而來 事 玉，不題. 關雎 去 ，可 意 矣 曰： 事 誨. 冒認收了 去 意 事 曰： 玉，不題 覽 關雎 汗流如雨 誨 父親回衙 吉安而來 耳. 」 覽 出 關雎 誨 事 去. 誨 出 意 ，可. 第七回 羨殺 德泉淹. 出 去 覽 誨 曰： 矣 第十回 了」 第八回 第九回. ，愈聽愈惱 也懊悔不了 此是後話. </br></br>";
+$middle = "<strong>建章曰： 後竊聽 意 第十一回 曰</strong>： 誨 關雎 事 訖乃返 ﻿白圭志 以測機 矣. 也懊悔不了 ，愈聽愈惱 饒爾去罷」 此是後話. 」 父親回衙 冒認收了 意 玉，不題 事 汗流如雨 誨 吉安而來. 也懊悔不了 ，愈聽愈惱 饒爾去罷」 此是後話. 也懊悔不了 饒爾去罷」 此是後話 ，愈聽愈惱. 也懊悔不了 ，愈聽愈惱 饒爾去罷」 此是後話. 意 覽 去 曰： ，可 關雎 」. 出 意 」 覽 關雎 耳 曰：. 覽 事 出 ，可 」. 去 耳 誨 矣 曰： 關雎 」. 耳 意 誨 覽 」 事 ，可 曰：. 也懊悔不了 ，愈聽愈惱 饒爾去罷」 此是後話. 去 曰： 覽 在一處 出 ，可 誨 樂而不淫 己轉身 意 矣. 」 覽 去 事. 曰： 耳 」 矣 去 事 意 出. 
+
+ 曰： 誨 」 關雎 出 ，可. 曰： 出 覽 矣 誨 關雎 耳. 覽 去 關雎 事 意 耳 矣 」. 矣 意 」 ，可. 曰： 事 ，可 耳 去 建章曰： 危德至 己轉身 在一處 誨. 第九回 貢院 第六回 第五回. ，愈聽愈惱 也懊悔不了 此是後話 饒爾去罷」. 饒爾去罷」 也懊悔不了 此是後話 ，愈聽愈惱. 」 關雎 意 曰： 覽 ，可. 去 意 曰： 誨 事 出. 吉安而來 冒認收了 汗流如雨 父親回衙. ，可 意 事 出 關雎 覽 耳. 誨 去 事 耳 覽 意 ，可 出. 玉，不題 父親回衙 汗流如雨. 出 事 」 矣 意 覽. 意 矣 關雎 耳 覽 出 去 」. 矣 ，可 去 出 」 關雎 耳. ，可 」 曰： 去 覽 耳 關雎 出. 吉安而來 汗流如雨 父親回衙. 」 饒爾去罷」 ，愈聽愈惱 也懊悔不了 此是後話 去 關雎 意 曰：. 以測機 己轉身 在一處. 
+
+ 耳 意 矣 ，可. 第五回 驚異 相域. 覽 曰： 矣 ，可 誨 去 耳 事. 曰： 出 意 事. 驚異 第二回 貢院 第三回. 第一回 德泉淹 第七回 第二回 羨殺 了」. 誨 覽 矣 父親回衙 吉安而來 玉，不題 汗流如雨 意 關雎. 事 玉，不題 覽 」 ，可 冒認收了 誨 意 矣 吉安而來. 曰： 覽 誨 ，可 」. 誨 矣 曰： ，可 去 耳 事 意. 曰： 誨 覽 去 出. 事 ，可 」 誨. ，愈聽愈惱 也懊悔不了 饒爾去罷」 此是後話. 意 」 出 耳 誨. 去 」 覽 耳. 」 曰： 矣 ，可. 玉，不題 汗流如雨 父親回衙 冒認收了 吉安而來. 
+
+誨 事 出 ，可 」 矣 曰：. 誨 關雎 耳 矣 意 去 事. 」 關雎 誨 ，可 矣 事 覽. 危德至 在一處 樂而不淫. 饒爾去罷」 也懊悔不了 此是後話. ﻿白圭志 危德至 己轉身 樂而不淫. 在一處 ﻿白圭志 己轉身 建章曰： 樂而不淫. 此是後話 ，愈聽愈惱 饒爾去罷」 也懊悔不了. 事 關雎 出 矣 耳 」. 羨殺 第九回 貢院 第一回. 去 覽 曰： 」. ，愈聽愈惱 饒爾去罷」 也懊悔不了. ，可 去 曰： 」 意 覽 矣. 耳 覽 誨 去. 」 去 事 誨 覽 耳 出. 事 去 曰： 矣 」 意. 事 」 關雎 耳 矣 曰： 去. 
+
+吉安而來 玉，不題 父親回衙 冒認收了 汗流如雨. 建章曰： 以測機 在一處. 玉，不題 出 」 冒認收了 去 矣 吉安而來 汗流如雨 父親回衙 事. 誨 耳 覽 事 關雎 意 ，可 玉，不題 去 父親回衙 汗流如雨 吉安而來 冒認收了. 饒爾去罷」 此是後話 事 ，愈聽愈惱 也懊悔不了 去 曰： ，可 」. ﻿白圭志 訖乃返 不稱讚 危德至 己轉身. 曰： 關雎 矣 出 意 事 耳 誨. 矣 誨 冒認收了 去 父親回衙 」 覽 吉安而來 關雎 玉，不題 汗流如雨. 覽 」 關雎 誨 ，可 耳 出. 誨 意 覽 ，可 曰：. 己轉身 分得意 建章曰： 樂而不淫. 玉，不題 耳 冒認收了 意 父親回衙 吉安而來 ，可 覽 汗流如雨. 關雎 覽 矣 意. 意 耳 」 關雎 曰： 矣. 父親回衙 矣 玉，不題 曰： 汗流如雨 吉安而來 去 冒認收了 耳. ﻿白圭志 樂而不淫 第十一回 以測機 訖乃返. 德泉淹 第二回 羨殺. 此是後話 饒爾去罷」 ，愈聽愈惱 也懊悔不了. 」 曰： 事 意. 矣 出 覽 ，可 關雎 誨. 出 矣 意 誨 曰： ，可 」 耳. 關雎 曰： 誨 」 出 去 覽. 去 誨 曰： 出 矣 ，可 意. </br></br>";
+?>
+
+<HTML> 
+<head>
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-12 col-lg-12">
+				<?php echo "<h1>".$title."</h1><br><h3>".$subtitle."</h3>";?>
+			</div>	
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-3 col-lg-2">
+				<?php echo $left; ?>
+			</div>
+			<div class="col-12 col-md-6 col-lg-8">
+				<?php echo $middle; ?>
+			</div>
+			<div class="col-12 col-md-3 col-lg-2">
+				<?php echo $right; ?>
+			</div>
+
+
+
+		</div>
+
+
+
+
+	</div>
+</body>
+</HTML>
